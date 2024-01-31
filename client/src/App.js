16ae8e42ad993bcb72b092d588c10568788ba14e
Andyonthego <andrea.cn.sy.z@gmail.com>
2024-01-31 23:51:21 +0800

    switch to webpack

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div> Home </div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
