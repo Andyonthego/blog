500fed5e0dcb5e0e9bbaeb58cf87999073a3d5a3
Andyonthego <andrea.cn.sy.z@gmail.com>
2024-02-04 00:52:19 +0800

    login and register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,6 +9,7 @@ const Login = () => {
         <input type='text' placeholder='username'/>
         <input type='password' placeholder='password'/>
         <button>Login</button>
+        <p>Wrong password or username!</p>
         <span>Don't have an account? Register <a href='/register'>here</a>.</span>
       </form>
     </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='auth'>
+      <h1>Register</h1>
+      <form>
+        <input required type='text' placeholder='username'/>
+        <input required type='email' placeholder='email'/>
+        <input required type='password' placeholder='password'/>
+        <button>Register</button>
+        <span>Alreacy have an account? Login <a href='/Login'>here</a>.</span>
+      </form>
+    </div>
   )
 }
 
